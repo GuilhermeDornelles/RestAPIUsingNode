3df59d0974645bd8cad2b63897b0b6fea5689d1b
Guilherme Dornelles <guilherme.dornelles@nubo.ai>
2022-05-27 04:03:04 -0300

    Adding getOne, update and delete routes

--- routes/routes.js
+++ routes/routes.js
@@ -29,18 +29,38 @@ router.get("/getAll", async (req, res) => {
 });
 
 //Get by ID Method
-router.get("/getOne/:id", (req, res) => {
-  res.send(req.params.id);
+router.get("/getOne/:id", async (req, res) => {
+  try {
+    const data = await Model.findById(req.params.id);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 //Update by ID Method
-router.patch("/update/:id", (req, res) => {
-  res.send("Update by ID API");
+router.patch("/update/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updateData = req.body;
+    const options = { new: true };
+
+    const result = await Model.findByIdAndUpdate(id, updateData, options);
+    res.send(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 //Delete by ID Method
-router.delete("/delete/:id", (req, res) => {
-  res.send("Delete by ID API");
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Model.findByIdAndDelete(id);
+    res.send(`Document '${data.name}' has been deleted`);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 module.exports = router;
